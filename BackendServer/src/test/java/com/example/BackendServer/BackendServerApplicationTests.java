6bdc667262076f9b53c883be88cd69b4577a1038
Jang2019 <asdf1263@kookmin.ac.kr>
2025-07-05 14:48:46 +0900

    Create

---
+++ BackendServer/src/test/java/com/example/BackendServer/BackendServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BackendServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
