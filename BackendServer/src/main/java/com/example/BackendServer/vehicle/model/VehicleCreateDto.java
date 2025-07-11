51878bb661460be095e98cb48883ba8694d8dd79
kjh0work <kjh-work@naver.com>
2025-07-11 16:46:08 +0900

    Add 차량 등록 api

---
+++ BackendServer/src/main/java/com/example/BackendServer/vehicle/model/VehicleCreateDto.java
@@ -0,0 +1,15 @@
+package com.example.BackendServer.vehicle.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+public class VehicleCreateDto {
+
+    private String vehicleNumber;
+    private String vehicleName;
+}
