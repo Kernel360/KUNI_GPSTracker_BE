2c5598e02f6c8abdbe56452607eabd11cc4e3f2e
Suhyeon Kim <kimsuehune@gmail.com>
2025-07-14 21:31:29 +0900

    feat: 차량 삭제 기능 구현

--- BackendServer/src/main/java/com/example/BackendServer/vehicle/controller/VehicleApiController.java
+++ BackendServer/src/main/java/com/example/BackendServer/vehicle/controller/VehicleApiController.java
@@ -5,13 +5,12 @@ import com.example.BackendServer.vehicle.db.VehicleEntity;
 import com.example.BackendServer.vehicle.model.VehicleCreateDto;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@Slf4j // 로그 사용을 위해 추가
 @RestController
 @RequestMapping("/api/management")
 @RequiredArgsConstructor
@@ -30,7 +29,17 @@ public class VehicleApiController {
                 .build();
 
         return ResponseEntity.status(HttpStatus.CREATED).body(result);
-
     }
+
+    // 삭제
+    @DeleteMapping("/delete/{vehicleNumber}")
+    public ResponseEntity<Void> deleteByVehicleNumber(@PathVariable String vehicleNumber) {
+        log.info("차량번호 삭제 요청: {}", vehicleNumber);
+
+        vehicleApiService.deleteByVehicleNumber(vehicleNumber);
+
+        return ResponseEntity.ok().build(); // 200 OK 반환
+    }
+
 
 }
--- BackendServer/src/main/java/com/example/BackendServer/vehicle/db/VehicleRepository.java
+++ BackendServer/src/main/java/com/example/BackendServer/vehicle/db/VehicleRepository.java
@@ -1,6 +1,12 @@
 package com.example.BackendServer.vehicle.db;
 
+import com.example.BackendServer.vehicle.db.VehicleEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface VehicleRepository extends JpaRepository<VehicleEntity, Long> {
+
+    // 삭제
+    Optional<VehicleEntity> findByVehicleNumber(String vehicleNumber);
 }
