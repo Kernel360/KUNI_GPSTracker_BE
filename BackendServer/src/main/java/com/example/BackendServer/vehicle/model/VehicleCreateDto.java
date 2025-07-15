438dfbde0dca5faccea83cf7b835832083ba425f
kimyoobin <55120730+KimYooBin1@users.noreply.github.com>
2025-07-15 16:23:08 +0900

    [Feat]: swagger 적용
    - swagger 라이브러리 설치
    - config 세팅
    - VehicleApiController api 명세 작성
    - response, request DTO @Schema 작성

--- BackendServer/src/main/java/com/example/BackendServer/vehicle/model/VehicleCreateDto.java
+++ BackendServer/src/main/java/com/example/BackendServer/vehicle/model/VehicleCreateDto.java
@@ -1,6 +1,8 @@
 package com.example.BackendServer.vehicle.model;
 
 import com.example.BackendServer.vehicle.db.VehicleEntity;
+
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
@@ -10,10 +12,12 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder(toBuilder = true)
+@Schema(description = "차량 생성 요청 DTO")
 public class VehicleCreateDto {
 
     @NotBlank
+    @Schema(description = "차량 번호", example = "12가3456")
     private String vehicleNumber;
-
+    @Schema(description = "차량 이름", example = "MERCEDES")
     private VehicleEntity.Type vehicleName;
 }
