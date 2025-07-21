31c2f1fed33418f446760b449abe4d8cc1086d1c
Jang2019 <54920299+Jang2019@users.noreply.github.com>
2025-07-21 16:43:53 +0900

    파라미터 사전 필터링

--- BackendServer/src/main/java/com/example/BackendServer/gpsRecord/db/GpsRecordRepository.java
+++ BackendServer/src/main/java/com/example/BackendServer/gpsRecord/db/GpsRecordRepository.java
@@ -2,12 +2,13 @@ package com.example.BackendServer.gpsRecord.db;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface GpsRecordRepository extends JpaRepository<GpsRecordEntity,Long> {
 
     @Query(value = """
-        SELECT gr.vehicle_id, gr.latitude, gr.longitude, gr.status, gr.oTime
+        SELECT gr.*
         FROM GpsRecord gr
         INNER JOIN (
             SELECT vehicle_id, MAX(oTime) AS max_time
@@ -15,6 +16,7 @@ public interface GpsRecordRepository extends JpaRepository<GpsRecordEntity,Long>
             GROUP BY vehicle_id
         ) latest
         ON gr.vehicle_id = latest.vehicle_id AND gr.oTime = latest.max_time
+        WHERE (:status IS NULL OR gr.status = :status)
         """, nativeQuery = true)
-    List<GpsRecordEntity> findLatestGpsForAllVehicles();
+    List<GpsRecordEntity> findLatestGpsForAllVehiclesByStatus(@Param("status") String status);
 }
