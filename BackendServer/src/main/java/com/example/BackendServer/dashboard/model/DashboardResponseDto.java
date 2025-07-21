f0566204711b4a946a301f8b08d52f5cb67db628
Jang2019 <54920299+Jang2019@users.noreply.github.com>
2025-07-21 16:29:40 +0900

    Setter 제거 및 long 형태 변환

--- BackendServer/src/main/java/com/example/BackendServer/dashboard/model/DashboardResponseDto.java
+++ BackendServer/src/main/java/com/example/BackendServer/dashboard/model/DashboardResponseDto.java
@@ -4,12 +4,11 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
-@Setter
 @Getter
 @Builder
 public class DashboardResponseDto {
-    private int vehicles;
-    private int active;
-    private int inactive;
-    private int inspect;
+    private Long vehicles;
+    private long active;
+    private long inactive;
+    private long inspect;
 }
