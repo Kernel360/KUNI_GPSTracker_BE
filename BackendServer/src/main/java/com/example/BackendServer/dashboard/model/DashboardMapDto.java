c2cee6f12cbde02f060b56b8a797c1fb86a26020
Jang2019 <54920299+Jang2019@users.noreply.github.com>
2025-07-21 16:31:07 +0900

    Setter 제거

--- BackendServer/src/main/java/com/example/BackendServer/dashboard/model/DashboardMapDto.java
+++ BackendServer/src/main/java/com/example/BackendServer/dashboard/model/DashboardMapDto.java
@@ -2,9 +2,7 @@ package com.example.BackendServer.dashboard.model;
 
 import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
 
-@Setter
 @Getter
 @Builder
 public class DashboardMapDto {
