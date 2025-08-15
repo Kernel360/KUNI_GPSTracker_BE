685daf27bcf6ff2ce5a629ea3d1fe9f03c7f337d
kjh0work <kjh-work@naver.com>
2025-08-15 17:16:13 +0900

    Consumer의 Main이름 수정

--- BackendServer/consumer/src/main/java/com/example/ConsumerMain.java
+++ BackendServer/consumer/src/main/java/com/example/ConsumerMain.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Main {
+public class ConsumerMain {
 
     public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
+        SpringApplication.run(ConsumerMain.class, args);
     }
 
 }
