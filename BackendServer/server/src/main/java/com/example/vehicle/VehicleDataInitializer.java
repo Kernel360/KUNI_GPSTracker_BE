b1b9b4e1d5a2bddb7f3fa5ee49da556ff199e2c7
synnbaek <100paek100@naver.com>
2025-08-19 16:18:07 +0900

    차량 종류 숫자 불가수정

--- BackendServer/server/src/main/java/com/example/vehicle/VehicleDataInitializer.java
+++ BackendServer/server/src/main/java/com/example/vehicle/VehicleDataInitializer.java
@@ -19,14 +19,16 @@ public class VehicleDataInitializer {
   private final VehicleRepository vehicleRepository;
 
   private final List<String> vehicleNumbers = List.of(
-      "37허9534", "77하9129", "47하7226", "18하9305", "42호3796", "89하8148", "25하2382", "88허2646", "64허1225", "99하5525",
-      "26호6540", "50호2768", "94호8121", "19하5914", "70하3860", "20호9563", "79허1985", "72하7318", "69하4844", "47호5239"
-
+      "37허9534", "77하9129", "47하7226", "18하9305", "42호3796",
+      "89하8148", "25하2382", "88허2646", "64허1225", "99하5525",
+      "26호6540", "50호2768", "94호8121", "19하5914", "70하3860",
+      "20호9563", "79허1985", "72하7318", "69하4844", "47호5239"
   );
 
   @PostConstruct
   public void initVehicles() {
-    if (vehicleRepository.count() > 0) return;  // 이미 데이터가 있다면 초기화하지 않음
+    if (vehicleRepository.count() > 0) return; // 이미 데이터가 있으면 초기화 안 함
+
     IntStream.range(0, vehicleNumbers.size()).forEach(i -> {
       String number = vehicleNumbers.get(i);
 
